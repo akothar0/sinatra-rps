9676fbfc730fc6e817c3c9755adcbad9b0256a15
Aditya Kothari <adityakothari99@gmail.com>
2025-01-31 01:25:14 +0000

    Final answer commit

--- app.rb
+++ app.rb
@@ -1,9 +1,41 @@
 require "sinatra"
 require "sinatra/reloader"
 
+def who_won(computer_played, user_played)
+  if computer_played == user_played
+    "Tie"
+  elsif (computer_played == "Rock" && user_played == "Scissors") ||
+        (computer_played == "Paper" && user_played == "Rock") ||
+        (computer_played == "Scissors" && user_played == "Paper")
+    "Computer_win"
+  else
+    "User_win"
+  end
+end
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:elephant)
+end
+
+options = ["rock", "paper", "scissors"]
+
+get("/rock") do
+  @computer_played = options.sample
+  @user_played = "rock"
+  @result = who_won @computer_played, @user_played
+  erb(:rock)
+end
+
+get("/paper") do
+  @computer_played = options.sample
+  @user_played = "paper"
+  @result = who_won @computer_played, @user_played
+  erb(:paper)
+end
+
+get("/scissors") do
+  @computer_played = options.sample
+  @user_played = "scissors"
+  @result = who_won @computer_played, @user_played
+  erb(:scissors)
 end
